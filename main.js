abea452ec20685e77dedaaa4d7016226fc186efd
Amen Ra <amenra@Amens-MacBook-Pro.local>
2020-06-15 20:29:38 -0400

    using the _.each method

--- main.js
+++ main.js
@@ -1,1 +1,29 @@
-console.log('Hi Lodash', _.isEqual(1, 1));
+// var native = [1, 2].forEach((item) => {
+//     console.log(item);
+// })
+
+// var lodash = _.each([1, 2], (item) => {
+//     console.log(item);
+// })
+
+// console.log(native, lodash);
+
+var items = {
+    1: {
+        name: 'Milk'
+    },
+    2: {
+        name: 'Bread'
+    }
+}
+
+// console.log(items.forEach);
+
+var arr = [];
+
+_.each(items, (item, index)=> {
+    console.log(item, index);
+    arr.push(item)
+})
+
+console.log(arr);
