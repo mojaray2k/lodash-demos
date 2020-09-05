57adcece5793c255171e5b45d0db8702616dadf5
Amen Moja Ra <mojaray2k@gmail.com>
2020-09-05 16:01:00 -0400

    finished _.map demos

--- main.js
+++ main.js
@@ -3,8 +3,11 @@
 var newArr = [1,2,3].map((item) => item);
 console.log(newArr);
 
-var newArrayLodash = _.map([{id: 1, name: 'John'}, {id: 2, name: 'Jack'}], (item) =>  item.id );
+// var newArrayLodash = _.map([{id: 1, name: 'John'}, {id: 2, name: 'Jack'}], (item) =>  item.id );
+// don't pass a anonymous function just pass the the key of the array you want to get
+var newArrayLodash = _.map([{id: 1, name: 'John'}, {id: 2, name: 'Jack'}], "id");
 console.log(newArrayLodash);
+
 
 var users = {
   1: {
@@ -18,3 +21,43 @@ var users = {
 var userNames = _.map(users, (user) => user.name)
 
 console.log(userNames);
+
+// Map Quiz
+var users = [
+  {
+    id: 1,
+    first_name: 'John',
+    status: 'active'
+  },
+  {
+    id: 2,
+    first_name: 'Mike',
+    status: 'inactive'
+  }
+]
+
+// transition the array to this below
+// var users = [
+//   {
+//     id: 1,
+//     firstName: 'John',
+//     isActive: true
+//   },
+//   {
+//     id: 2,
+//     firstName: 'Mike',
+//     isActive: false
+//   }
+// ]
+
+function normalizeUsers (users) {
+  return _.map(users, (user) => {
+    return {
+      id: user.id,
+      firstName: user.first_name,
+      isActive: user.status === 'active'
+    }
+  } )
+}
+
+console.log(normalizeUsers(users));
